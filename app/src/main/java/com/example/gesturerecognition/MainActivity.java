85840ec2b26eb30ffe58dd0a57e4ad471490d0bb
Antonio Pugliese <antonio.pugliese98@hotmail.com>
2020-12-17 18:13:40 +0100

    Aggiunto salvataggio txt con i dati del sensore sullo storage

--- app/src/main/java/com/example/gesturerecognition/MainActivity.java
+++ app/src/main/java/com/example/gesturerecognition/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.gesturerecognition;
 
+import android.Manifest;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +17,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.mbientlab.metawear.Data;
 import com.mbientlab.metawear.DeviceInformation;
@@ -26,7 +30,19 @@ import com.mbientlab.metawear.builder.RouteComponent;
 import com.mbientlab.metawear.data.Acceleration;
 import com.mbientlab.metawear.module.Accelerometer;
 import com.mbientlab.metawear.module.Led;
+import com.opencsv.CSVWriter;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import bolts.Continuation;
 import bolts.Task;
@@ -38,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
 
     private MetaWearBoard board;
     private Accelerometer accelerometer;
+    private ArrayList<JSONObject> dataSensor = new ArrayList<JSONObject>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,6 +119,11 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
     }
 
     public void blinkLedTenTimes() {
+        if (!board.isConnected()){
+            Toast.makeText(MainActivity.this, "Please connect your sensor first", Toast.LENGTH_LONG).show();
+            return;
+        }
+
         Led led;
         if ((led = board.getModule(Led.class)) != null) {
             led.editPattern(Led.Color.BLUE, Led.PatternPreset.BLINK)
@@ -159,8 +181,21 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
                 source.stream(new Subscriber() {
                     @Override
                     public void apply(Data data, Object... env) {
-                        Log.i("stream", data.value(Acceleration.class).toString());
-                    }
+                        Log.i("board", data.value(Acceleration.class).toString());
+                        Log.i("board", String.valueOf(data.timestamp().getTimeInMillis()));
+
+                        JSONObject object = new JSONObject();
+                        try {
+                            object.put("timestamp", String.valueOf(data.timestamp().getTimeInMillis()));
+                            object.put("x", String.valueOf(data.value(Acceleration.class).x()));
+                            object.put("y", String.valueOf(data.value(Acceleration.class).y()));
+                            object.put("z", String.valueOf(data.value(Acceleration.class).z()));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        dataSensor.add(object);
+                }
                 });
             }
         }).continueWith(new Continuation<Route, Void>() {
@@ -176,11 +211,70 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
         Toast.makeText(MainActivity.this, "Measurement started", Toast.LENGTH_SHORT).show();
     }
 
-    public void stopAccMeasurement() {
+    public void stopAccMeasurement(){
         if(board == null || accelerometer == null) { return; }
 
         accelerometer.stop();
         Toast.makeText(MainActivity.this, "Measurement stopped", Toast.LENGTH_SHORT).show();
+
+        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            (new Handler()).postDelayed(this::writeDataOnDevice, 200);
+        } else {
+            (new Handler()).postDelayed(this::writeDataOnDevice, 200);
+        }
+
+
+
+
+        /*File file = new File(android.os.Environment.getExternalStorageDirectory().getAbsolutePath());
+        try {
+            FileWriter outputfile = new FileWriter(file);
+            CSVWriter writer = new CSVWriter(outputfile);
+
+            List<String[]> csvData = new ArrayList<String[]>();
+            csvData.add(new String[] {"timestamp", "x-axis", "y-axis", "z-axis" });
+
+            for(JSONObject x : dataSensor){
+                Log.d("board", x.toString());
+                csvData.add(new String[] {(String) x.get("timestamp"), (String) x.get("x"), (String) x.get("y"), (String) x.get("z")});
+            }
+
+            writer.writeAll(csvData);
+            writer.close();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    public void writeDataOnDevice() {
+        File dir = new File(android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download", "MyAppDataSensor");
+        if(!dir.exists()){
+            dir.mkdir();
+        }
+
+        try {
+            File gpxfile = new File(dir, "Data Sensor");
+            FileWriter writer = new FileWriter(gpxfile);
+
+            writer.append("timestamp, x-axis, y-axis, z-axis\n");
+            
+            for(JSONObject a : dataSensor){
+                //Log.d("board", a.toString());
+                String timestamp = (String) a.get("timestamp") + ", ";
+                String x = (String) a.get("x") + ", ";
+                String y = (String) a.get("y") + ", ";
+                String z = (String) a.get("z") + "\n";
+                writer.append((timestamp + x + y + z));
+            }
+            writer.flush();
+            writer.close();
+
+            Toast.makeText(MainActivity.this, "Your file ha been saved in folder " + dir, Toast.LENGTH_LONG).show();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
